e2324ee55e312ad3504a347abf5bdffbb8bf93cb
David Brookes <david@davidbrookes.io>
2017-09-25 18:47:18 +0100

    Initial add of project

---
+++ lambda/ContactForm/config.js
@@ -0,0 +1,11 @@
+﻿"use strict";
+
+var config = {
+    "templateBucket" : "davidbrookes.co.uk",
+    "templateKey" : "assets/email/template.html",
+    "targetAddress" : "david@davidbrookes.co.uk",
+    "fromAddress": "David Brookes <david@davidbrookes.co.uk>",
+    "defaultSubject" : "Freelance enquiry: {{name}}"
+}
+
+module.exports = config
