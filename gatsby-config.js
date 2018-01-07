7f52dfdab75646f0a8af14dd68bd5d9aa2187eb0
David Brookes <david@davidbrookes.io>
2018-01-07 16:56:10 +0000

    add service worker and manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,55 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `David Brookes`,
+        short_name: `dbrookes`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
+        display: `minimal-ui`,
+        icons: [
+          {
+            src: `/favicons/android-chrome-36x36.png`,
+            sizes: `36x36`,
+            type: `image/png`,
+            density: `0.75`,
+          },
+          {
+            src: `/favicons/android-chrome-48x48.png`,
+            sizes: `48x48`,
+            type: `image/png`,
+            density: `1.0`,
+          },
+          {
+            src: `/favicons/android-chrome-72x72.png`,
+            sizes: `72x72`,
+            type: `image/png`,
+            density: `1.5`,
+          },
+          {
+            src: `/favicons/android-chrome-96x96.png`,
+            sizes: `96x96`,
+            type: `image/png`,
+            density: `2.0`,
+          },
+          {
+            src: `/favicons/android-chrome-144x144.png`,
+            sizes: `144x144`,
+            type: `image/png`,
+            density: `3.0`,
+          },
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+            density: `4.0`,
+          },
+        ],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
@@ -26,6 +75,7 @@ module.exports = {
         ],
       },
     },
+    `gatsby-plugin-offline`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
